80802b24c1d9b262972e10ea067ca0db02077fb2
Sekionn <Sjknudesn@gmail.com>
2026-02-12 14:12:53 +0100

    Current velo pack implementation

---
+++ price-bot/Updater/VelopackUpdaterService.cs
@@ -0,0 +1,37 @@
+﻿using Velopack;
+
+namespace price_bot.Updater
+{
+    public class VelopackUpdaterService
+    {
+        const string updatePath = "https://the.place/you-host/updates";
+
+        public static async Task<bool> CheckForUpdates()
+        {
+            var mgr = new UpdateManager(updatePath);
+
+            // check for new version
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion == null)
+                return false; // no update available
+
+            return true;
+        }
+
+        public static async Task ApplyUpdate()
+        {
+            var mgr = new UpdateManager(updatePath);
+
+            // check for new version
+            var newVersion = await mgr.CheckForUpdatesAsync();
+            if (newVersion == null)
+                return; // no update available
+
+            // download new version
+            await mgr.DownloadUpdatesAsync(newVersion);
+
+            // install new version and restart app
+            mgr.ApplyUpdatesAndRestart(newVersion);
+        }
+    }
+}
---
+++ price-bot/Verification/VerificationService.cs
@@ -0,0 +1,25 @@
+﻿using price_bot.FileReaders;
+using price_bot.Models;
+
+namespace price_bot.Verification;
+public class VerificationService
+{
+    public static async Task UpdateLicenseData(License data)
+    {
+        await FileWriter.FileWriter.WriteJSONFile(data, "license");
+
+        return;
+    }
+
+    public static License? GetLicenseData()
+    {
+        return FileReader.ReadJson<License>("license");
+    }
+
+    public static int VerifyLicense()
+    {
+        var license = GetLicenseData();
+
+        return 1;
+    }
+}
