4608de1219a0b51ed12dace29857ebe0048ea2e9
Sebastian <Sjknudesn@gmail.com>
2026-02-13 15:42:00 +0100

    now opens files once finished or if missing file
    also has new commands for opening those folders

--- price-bot/Program.cs
+++ price-bot/Program.cs
@@ -1,15 +1,17 @@
+using NPOI.HPSF;
 using price_bot.Demo_feature;
 using price_bot.FileWriter;
 using price_bot.Logging;
 using price_bot.Networking;
 using price_bot.Updater;
+using System.Diagnostics;
 using Velopack;
 internal class Program
 {
     private static async Task Main(string[] args)
     {
         VelopackApp.Build().Run();
-
+#if !DEBUG
         bool updateReady = await VelopackUpdaterService.CheckForUpdates();
 
         if (updateReady)
@@ -17,7 +19,7 @@ internal class Program
             Console.WriteLine("We are updating...");
             await VelopackUpdaterService.ApplyUpdate();
         }
-
+#endif
         var versionRetreiver = new VersionRetreiver();
         var demoController = new DemoController();
 
@@ -32,89 +34,118 @@ internal class Program
         }
 
         var logger = new LoggingService<Program>();
-        string? command = null;
-
-        if (!versionRetreiver.IsDemoVersion() || !demoController.HasDemoFinished())
+        string command = "";
+        while (!command!.Equals("exit", StringComparison.CurrentCultureIgnoreCase))
         {
-            Console.WriteLine("Vil du gerne have tjekket bog og ide's priser?");
-            Console.WriteLine("Skriv 'Ja' for at starte programmet");
+            Console.Clear();
 
-            command = Console.ReadLine();
-        }
-        else
-        {
-            Console.WriteLine("Din proeveperiode er udloebet kontakt Sebastian for at koebe programmet");
-        }
-
-        if (command != null)
-        {
-            if (command.Equals("ja", StringComparison.CurrentCultureIgnoreCase))
+            if (!versionRetreiver.IsDemoVersion() || !demoController.HasDemoFinished())
             {
-                bool timeNotSet = true;
-                int delayInHours = 0;
-                while (timeNotSet)
-                {
-                    Console.WriteLine("Hvor mange timer skal programmet vente før den starter?");
-                    Console.WriteLine("skriv et tal");
-                    var selectedDelay = Console.ReadLine();
+                Console.WriteLine("Vil du gerne have tjekket bog og ide's priser?");
+                Console.WriteLine("Du har nu følgende kommandoer:");
+                Console.WriteLine("start (for at starte programmet)");
+                Console.WriteLine("excel (for at tilføje en ny excel fil)");
+                Console.WriteLine("forkert (åbner mappen hvor den generede fil med de forkerte priser ligger)");
+                Console.WriteLine("exit (dette vil slukke programmet)");
 
-                    if (selectedDelay != null)
+                command = Console.ReadLine()!;
+            }
+            else
+            {
+                Console.WriteLine("Din prøveperiode er udløbet kontakt Sebastian for at købe programmet");
+                command = "exit";
+            }
+
+            if (command != null)
+            {
+                var files = Directory.GetFiles(Path.GetFullPath(@"Filer"), "*.xlsx").Length;
+
+                while (files <= 0 || command!.Equals("excel", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    var applicationDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                    Process.Start(new ProcessStartInfo { FileName = Path.GetFullPath(@"Filer"), UseShellExecute = true });
+
+                    Console.WriteLine("Du kan nu tilføje din excel fil, når dette er gjort kan du skrive start for at starte programmet");
+                    command = Console.ReadLine()!;
+
+                    files = Directory.GetFiles(Path.GetFullPath(@"Filer"), "*.xlsx").Length;
+                }
+
+                if (command.Equals("forkert", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Process.Start(new ProcessStartInfo { FileName = Path.GetFullPath(@"Forkerte priser"), UseShellExecute = true });
+                }
+                else if (command.Equals("start", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    bool timeNotSet = true;
+                    int delayInHours = 0;
+                    while (timeNotSet)
                     {
-                        if (int.TryParse(selectedDelay, out int r) == true)
+                        Console.WriteLine("Hvor mange timer skal programmet vente før den starter?");
+                        Console.WriteLine("skriv et tal");
+                        var selectedDelay = Console.ReadLine();
+
+                        if (selectedDelay != null)
                         {
-                            delayInHours = r;
-                            timeNotSet = false;
-                            Console.ForegroundColor = ConsoleColor.Black;
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.WriteLine($"Tallet er accepteret, systemet venter nu i {delayInHours} time(r)");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.BackgroundColor = ConsoleColor.Black;
+                            if (int.TryParse(selectedDelay, out int r) == true)
+                            {
+                                delayInHours = r;
+                                timeNotSet = false;
+                                Console.ForegroundColor = ConsoleColor.Black;
+                                Console.BackgroundColor = ConsoleColor.Green;
+                                Console.WriteLine($"Tallet er accepteret, systemet venter nu i {delayInHours} time(r)");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.BackgroundColor = ConsoleColor.Black;
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Black;
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Det der blev tastet var ikke et gyldigt tal, prøv igen");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.BackgroundColor = ConsoleColor.Black;
+
+                            }
                         }
                         else
                         {
                             Console.ForegroundColor = ConsoleColor.Black;
                             Console.BackgroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Det der blev tastet var ikke et gyldigt tal, prøv igen");
+                            Console.WriteLine("Der er ikke tastet noget tal i inputtet");
                             Console.ForegroundColor = ConsoleColor.White;
                             Console.BackgroundColor = ConsoleColor.Black;
-
                         }
                     }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Der er ikke tastet noget tal i inputtet");
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.BackgroundColor = ConsoleColor.Black;
-                    }
+
+                    var countdown = new price_bot.Timer.Timer(delayInHours);
+
+                    await countdown.Countdown();
+
+                    DateTime startDate = DateTime.Now;
+
+                    ProductRetreiver productRetreiver = new();
+                    var incorrectProducts = await productRetreiver.GetProductsWithIncorrectPrices();
+
+                    DirectoryInfo di = Directory.CreateDirectory("Forkerte priser");
+                    logger.CreateLog($"The directory was created successfully at {Directory.GetCreationTime("Forkerte priser")}.");
+
+                    FileWriter fileWriter = new();
+                    await fileWriter.WriteTXTFile(incorrectProducts);
+                    await fileWriter.WriteExcelFile(incorrectProducts);
+
+                    Process.Start(new ProcessStartInfo { FileName = Path.GetFullPath(@"Forkerte priser"), UseShellExecute = true });
+                    Process.Start(new ProcessStartInfo { FileName = Path.GetFullPath(@"Forkerte priser\Forkerte priser.xls"), UseShellExecute = true });
+
+                    DateTime finishDate = DateTime.Now;
+
+                    logger.CreateLog($"Start tidspunkt: {startDate}");
+                    logger.CreateLog($"Slut tidspunkt: {finishDate}");
+                    logger.CreateLog($"Hvor lang tid tog det: {finishDate - startDate}");
                 }
-
-                var countdown = new price_bot.Timer.Timer(delayInHours);
-
-                await countdown.Countdown();
-
-                DateTime startDate = DateTime.Now;
-
-                ProductRetreiver productRetreiver = new();
-                var incorrectProducts = await productRetreiver.GetProductsWithIncorrectPrices();
-
-                DirectoryInfo di = Directory.CreateDirectory("Forkerte priser");
-                logger.CreateLog($"The directory was created successfully at {Directory.GetCreationTime("Forkerte priser")}.");
-
-                FileWriter fileWriter = new();
-                await fileWriter.WriteTXTFile(incorrectProducts);
-                await fileWriter.WriteExcelFile(incorrectProducts);
-
-                DateTime finishDate = DateTime.Now;
-
-                logger.CreateLog($"Start tidspunkt: {startDate}");
-                logger.CreateLog($"Slut tidspunkt: {finishDate}");
-                logger.CreateLog($"Hvor lang tid tog det: {finishDate - startDate}");
             }
         }
-
         Console.WriteLine("Farvel");
-        Console.ReadLine();
+        Thread.Sleep(100);
     }
 }
